402540c0271cf2e77566fa8de055eca62e4e8987
gustavochavesferreira <gchavesf04@gmail.com>
2024-12-09 17:03:52 -0300

    Adicionando mais 3 testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,46 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  // Novos testes de integração a partir daqui:
+
+  it('Não permite a inserção de uma tarefa sem texto', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('{enter}'); 
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+
+  it('Não adiciona nenhuma tarefa, filtra por tarefas completas e retorna uma lista vazia', () => {
+    cy.visit(''); 
+  
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+  
+    cy.get('[data-cy=filter-completed-link]').click();
+  
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .should('have.length', 0);
+  });
+  
+  it('Alterna o status de tarefa entre completa e ativa', () => {
+    cy.visit('');
+    cy.get('[data-cy=todo-input]').type('Tarefa 1{enter}');
+  
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]').click();
+    cy.get('[data-cy=filter-completed-link]').click();
+    cy.get('[data-cy=todos-list]').children().should('have.length', 1);
+  
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]').click();
+    cy.get('[data-cy=filter-active-link]').click();
+    cy.get('[data-cy=todos-list]').children().should('have.length', 1);
+  });
+  
+  
 });
